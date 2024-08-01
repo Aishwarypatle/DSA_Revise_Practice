61c5320bc847ef111b607d6e8b47c7968853689c
Aishwary <aishwarypatle@26gmail.com>
2024-08-01 23:16:42 +0530

    basic

---
+++ JAVA/Basic/Hello.java
@@ -0,0 +1,11 @@
+package JAVA.Basic;
+
+import java.util.Scanner;
+
+class Hello {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        System.out.println(input.nextLine());
+    }
+    
+}
